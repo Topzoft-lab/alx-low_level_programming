fed1ea70a51b3eb50deb9ea70110668178f165a2
Topzoft-lab <omiwoletemitope@gmail.com>
2023-09-27 02:12:38 -0700

    new changes

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,46 +1,50 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * add_node - adds a node at the beginning of the linked list
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to the pointer to the head of the list.
+ * @str: String to be duplicated and added to the new node.
  *
- * @head: pointer to the first node of the list
- * @str: string to add to new node in the list
- *
- * Return: NULL if it fails / starting addr of the list
-*/
-
-list_t *add_node(list_t **head, const char *str)
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t str_len = 0;
+	list_t *new_node, *current;
 
-	/*set strig length to 0 if its NULL*/
 	if (str == NULL)
-		str_len = 0;
-
-	/*count length of string*/
-	while (str[str_len] != '\0')
-		str_len++;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
 		return (NULL);
 
-	/**
-	 * initialize the next addr of new_node
-	 * to NULL if head is NULL, if it's not
-	 * move the first node to the addr of new_node
-	 * ->next hence adding new_node at the beginning
-	 *  of the list
-	*/
+	new_node = malloc(sizeof(list_t)); /* Allocate memory for the new node */
+
+	if (new_node == NULL)
+		return (NULL); /* Return NULL if malloc fails */
+
+	new_node->str = strdup(str); /* Duplicate the input string */
+
+	if (new_node->str == NULL)
+	{
+		free(new_node); /* Free the new_node if strdup fails */
+		return (NULL);
+	}
+
+	new_node->len = strlen(str); /* Set the length of the string */
+	new_node->next = NULL; /* Initialize the next pointer for the new node */
+
 	if (*head == NULL)
-		new_node->next = NULL;
+	{
+		*head = new_node; /* If the list is empty, make the new node the head */
+	}
 	else
-		new_node->next = *head;
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next; /* Traverse to the last node */
+		}
+		current->next = new_node; /* Add the new node to the end of the list */
+	}
 
-	new_node->str = strdup(str);
-	new_node->len = str_len;
-	*head = new_node;
-
-	return (*head);
+	return (new_node);
 }
